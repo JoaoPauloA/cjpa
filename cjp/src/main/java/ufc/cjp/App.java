959150f2a1319194926f8d336eaeb5612e2fa4d7
JoaoPauloA <perseu456@gmail.com>
2017-12-15 14:00:03 -0300

    adicionados

---
+++ cjp/src/main/java/ufc/cjp/App.java
@@ -0,0 +1,13 @@
+package ufc.cjp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
